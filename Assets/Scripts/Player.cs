b22aac9b025823d84f32009c6d35d1413084e3e4
Conway Xu <conwayxu93@gmail.com>
2022-01-31 00:45:26 -0500

    base model of abilities

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,10 +10,7 @@ public class Player : MonoBehaviour
 {
 
     [SerializeField] private float speed=3f;
-    [SerializeField] private List<Ability> abilities;
-    [SerializeField] public AbilityManager abilityManager;
-    
-    [SerializeField] private float speed = 3f;
+    [SerializeField] public List<Ability> abilities = new List<Ability>{};
     [SerializeField] private float jumpForce = 3f;
 
     private Rigidbody2D rb;
@@ -75,6 +72,24 @@ public class Player : MonoBehaviour
     public void FixedUpdate()
     {
         rb.velocity = new Vector2(x_vel, rb.velocity.y);
+        for(int i = 0; i < abilities.Count; i++){
+            abilities[i].currentCooldown -= Time.deltaTime;
+            if(abilities[i].currentCooldown < 0){
+                abilities[i].currentCooldown = 0;
+            }
+        }
     }
 
+    public void OnUseAbility(InputValue input){
+        abilities[0].action();
+    }
+
+    public void putOnCooldown(Ability ability){
+        if(!abilities.Contains(ability)){
+            return;
+        }
+        else {
+            ability.currentCooldown = ability.getCooldown();
+        }
+    }
 }
