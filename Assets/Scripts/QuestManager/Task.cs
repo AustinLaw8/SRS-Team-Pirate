770052de058aaf46b42353d26332d14c39b5af26
Jennifer <jennifer@MacBook-Pro-5.local>
2022-02-04 21:01:52 -0800

    Added Task functions

--- Assets/Scripts/QuestManager/Task.cs
+++ Assets/Scripts/QuestManager/Task.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class Task : MonoBehaviour
+public abstract class Task : ScriptableObject
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    public string taskDescription;
+    public int reqAmount;
+    public bool completed{get; protected set;}
+    public int curAmount;
+    public TaskCompletionEvent completionEvent;
+
+    public void initialize() {
+        completed = false;
+        curAmount = 0;
+        completionEvent = new TaskCompletionEvent();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public bool checkTasks() {
+        if (curAmount >= reqAmount) {
+            complete();
+            return true;
+        }
+        else return false;
     }
+
+    void complete() {
+        completed = true;
+        completionEvent.Invoke(this);
+    }
+
+    public string getDescription() {
+        return taskDescription;
+    }
+
+    
+
+    
 }
+
+public class TaskCompletionEvent: UnityEvent<Task> {}
