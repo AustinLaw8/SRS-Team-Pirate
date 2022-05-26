c8d51918be126d37e7cd5fca6a8bfe6b07fec5bb
Austin Law <austinlaw8@gmail.com>
2022-05-26 08:19:37 -0700

    Main menu, credits set up but not written in yet

---
+++ Assets/Scripts/PlayButton.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayButton : MonoBehaviour
+{   
+    // Update is called once per frame
+    public void OnClick ()
+    {
+        SceneManager.LoadScene("BeachZone");
+    }
+}
